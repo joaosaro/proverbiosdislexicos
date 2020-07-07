6720b7af6a9a4c86bcbc46a2bce7abf8f5028cb0
João Saro <joaovlsaro@gmail.com>
2020-07-08 00:03:56 +0100

    Add Button stories

---
+++ src/stories/ColorButton.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ColorButton from "../components/color-button/ColorButton";
+
+export const Default = () => (
+  <>
+    <ColorButton color="steelblue" />
+    <ColorButton color="red" />
+    <ColorButton color="orange" />
+    <ColorButton color="yellow" />
+  </>
+);
+
+export const Active = () => (
+  <>
+    <ColorButton color="steelblue" />
+    <ColorButton color="red" active />
+  </>
+);
+
+export default {
+  title: "ColorButton",
+  component: ColorButton
+};
