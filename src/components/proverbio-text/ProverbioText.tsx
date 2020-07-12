a79cb8a184f8b0e4679525f05b9a0750e619060e
João Saro <joaovlsaro@gmail.com>
2020-07-12 18:06:53 +0100

    Open style and other props on ProverbioText

--- src/components/proverbio-text/ProverbioText.tsx
+++ src/components/proverbio-text/ProverbioText.tsx
@@ -1,22 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, HTMLAttributes } from "react";
 import "./proverbio-text.scss";
 import { ProverbioContext } from "../../providers/Proverbio";
 
 type AlignY = "top" | "bottom";
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLParagraphElement> {
   alignY?: AlignY;
 }
 
-const ProverbioText: React.FC<Props> = ({ children, alignY = "top" }) => {
+const ProverbioText: React.FC<Props> = ({
+  children,
+  alignY = "top",
+  style,
+  ...props
+}) => {
   const { editable } = useContext(ProverbioContext);
   const alignSelf = alignY === "top" ? "start" : "end";
 
   return (
     <p
       className="proverbio-text"
-      style={{ alignSelf }}
+      style={{ alignSelf, ...style }}
       contentEditable={editable}
+      {...props}
     >
       {children}
     </p>
